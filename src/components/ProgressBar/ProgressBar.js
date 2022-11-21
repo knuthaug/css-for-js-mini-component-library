51b1ad2dc3cb388ed1f8a1fb7cd7f031c5eae931
Knut Haugen <knuthaug+github@gmail.com>
2022-11-21 13:06:47 +0100

    progressbar done

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,62 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const sizes = {
+  small: {
+    "--progressbarHeight": "8px",
+    "--contentHeight": "8px",
+    "--borderRadius": "4px",
+  },
+  medium: {
+    "--progressbarHeight": "12px",
+    "--contentHeight": "12px",
+    "--borderRadius": "4px",
+  },
+  large: {
+    "--borderRadius": "8px",
+    "--progressbarHeight": "24px",
+    "--contentHeight": "16px",
+    padding: "4px",
+  },
+};
+
+const ProgressBarElement = styled.div`
+  height: var(--progressbarHeight);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--borderRadius);
+  overflow: hidden;
+`;
+
+const ProgressContent = styled.div`
+  background-color: ${COLORS.primary};
+  height: var(--contentHeight);
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = sizes[size];
+
+  const contentStyles = {
+    width: `${value}%`,
+    "border-radius": "4px 0px 0px 4px",
+  };
+
+  return (
+    <ProgressBarElement
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={styles}
+    >
+      <VisuallyHidden>{value}</VisuallyHidden>
+      <ProgressContent style={contentStyles}></ProgressContent>
+    </ProgressBarElement>
+  );
 };
 
 export default ProgressBar;
